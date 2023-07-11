00e7dd19d73c9e5cac91a1e76ce7a172468bfd00
amarjeet-arora <amarjeet.sn1@gmail.com>
2023-07-11 18:47:02 +0530

    changed again2

--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,6 @@
 
 public class Welcome {
     public static void main(String args[]){
-        System.out.println("welcome to java from Jenkins from git change-1");
+        System.out.println("welcome to java from Jenkins from git change-2");
     }
 }
